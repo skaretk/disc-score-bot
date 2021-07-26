4b7958ad061c40a8ae3fc365cae3a87f7c05f262
KJETILSTA\Kjetil <kskaret@gmail.com>
2021-07-26 21:13:32 +0200

    Add server and channel folder if not exist Add status for current channel

--- example_bot.py
+++ example_bot.py
@@ -1,3 +1,4 @@
+import os
 import discord
 import logging
 import csv
@@ -22,38 +23,49 @@ async def on_message(message):
         return
 
     channel = str(message.channel)
-    author=str(message.author)
-    if channel == "bot":
-        if message.attachments:
-            print("There is an attachment")            
-            for attachment in message.attachments:
-                print("{0} attached!".format(attachment.content_type))
-                if 'text/csv' in attachment.content_type:
-                    print("csv attached!")                    
-                    await attachment.save(fp="csv/{}".format(attachment.filename)) # saves the file
-                    await message.channel.send('.csv file {} received from {} and stored!'.format(attachment.filename, author))
-                    print("csv attached and stored in csv/{}!".format(attachment.filename)) 
+    author = str(message.author)
+    currentPath = str('{}\{}'.format(message.guild.name, channel))
 
-                    with open('csv/{}'.format(attachment.filename)) as csv_file:
-                        reader = csv.DictReader(csv_file)
-                        line_count = 0
-                        message_to_send = ''
-                        for row in reader:
-                            if line_count == 0:
-                                print("{} {} {}".format(row['CourseName'], row['Date'], row['Total'])) 
-                                message_to_send += "{} {} {}\n".format(row['CourseName'], row['Date'], row['Total'])
-                            else:
-                                print("{} {} {}".format(row['PlayerName'], row['Total'], row['+/-']))
-                                message_to_send += "\n{} {} {}".format(row['PlayerName'], row['Total'], row['+/-'])
-                            line_count += 1
-                        await message.channel.send(message_to_send)
+    # any attachemtns in the message?
+    if message.attachments:
+        for attachment in message.attachments:
+            if 'text/csv' in attachment.content_type:
+                print("csv attached from server {} channel {}".format(message.guild.name, channel))
+                if not os.path.exists(currentPath):
+                    os.makedirs(currentPath)
+                await attachment.save(fp="{}\{}".format(currentPath, attachment.filename)) # saves the file in a server/channel folder
+                await message.channel.send('{} attached {}'.format(attachment.filename, author))
+                print("csv attached and stored in {}\{}!".format(currentPath, attachment.filename)) 
 
-                else:
-                    print("Not a .csv?")
-        else:
-            print("There is no attachment")
+                with open('{}\{}'.format(currentPath, attachment.filename)) as csv_file:
+                    reader = csv.DictReader(csv_file)
+                    line_count = 0
+                    msg_to_send = ''
+                    for row in reader:
+                        if line_count == 0:
+                            print("{} {} {}".format(row['CourseName'], row['Date'], row['Total'])) 
+                            msg_to_send += "{} {} {}\n".format(row['CourseName'], row['Date'], row['Total'])
+                        else:
+                            print("{} {} {}".format(row['PlayerName'], row['Total'], row['+/-']))
+                            msg_to_send += "\n{} {} {}".format(row['PlayerName'], row['Total'], row['+/-'])
+                        line_count += 1
+                await message.channel.send(msg_to_send)
+
+    # Check status for the current channel
+    elif message.content == '%status':        
+        if not os.path.exists(currentPath):
+            await message.channel.send("No files stored for this channel")
+            return
+        
+        msg_to_send = ''
+        file_count = 0
+        for file in os.listdir(currentPath):
+            if file.endswith(".csv"):
+                file_count += 1
+                print(os.path.join("{}\{}".format(currentPath, file)))
+                msg_to_send += "\n{}".format(file)
+        await message.channel.send("No of files: {}\n{}".format(file_count, msg_to_send))
 
 #@client.event
 
 client.run('ODY4MjAxODY0MDA3NjU5NTYx.YPsN_g.5gkeaR6HIzCjxIyMcgLksOoSbgk')
-
