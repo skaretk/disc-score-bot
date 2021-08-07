88dacb862faa10eadadcad1666c464ab4cd2e28d
Kjetil Skaret <kskaret@gmail.com>
2021-08-07 23:36:22 +0200

    Use csv.line_num instead of counting manually

--- csvreader.py
+++ csvreader.py
@@ -6,27 +6,24 @@ from scorecard import Scorecard
 class CsvReader:
     def __init__(self, path, file):
         self.path = path
-        self.file = file       
+        self.file = file
     
     def parse(self):
         with open(f'{self.path}\{self.file}', encoding='UTF-8', newline='') as csv_file:
             reader = csv.DictReader(csv_file)
-            line_count = 0
             for row in reader:
-                if line_count == 0:
+                if reader.line_num == 2:
                     scorecard = Scorecard(row['CourseName'], row['Date'], int(row['Total']) )
                 else:
                     player = Player(row['PlayerName'], int(row['Total']), int(row['+/-']))
                     scorecard.add_player(player)
-                line_count += 1
         return scorecard
     
     def parse_course(self, course):
         with open(f'{self.path}\{self.file}', encoding='UTF-8', newline='') as csv_file:
             reader = csv.DictReader(csv_file)
-            line_count = 0
             for row in reader:
-                if line_count == 0:
+                if reader.line_num == 2:
                     if course.lower() in row['CourseName'].lower():
                         scorecard = Scorecard(row['CourseName'], row['Date'], int(row['Total']) )
                     else:
@@ -34,15 +31,13 @@ class CsvReader:
                 else:
                     player = Player(row['PlayerName'], int(row['Total']), int(row['+/-']))
                     scorecard.add_player(player)
-                line_count += 1
         return scorecard
 
     def parse_dates(self, date, date_to = ''):
         with open(f'{self.path}\{self.file}', encoding='UTF-8', newline='') as csv_file:
             reader = csv.DictReader(csv_file)
-            line_count = 0
             for row in reader:
-                if line_count == 0:                    
+                if reader.line_num == 2:                  
                     scorecard_date = datetime.datetime.strptime(row['Date'],'%Y-%m-%d %H:%M')
                     # Parse scores between two dates ?
                     if (date_to):
@@ -64,5 +59,4 @@ class CsvReader:
                 else:
                     player = Player(row['PlayerName'], int(row['Total']), int(row['+/-']))
                     scorecard.add_player(player)
-                line_count += 1
         return scorecard
