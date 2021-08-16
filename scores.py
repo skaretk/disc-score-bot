69dc132e526adb6741f3b42d737297cd116ec3a9
Kjetil Skaret <kskaret@gmail.com>
2021-08-16 22:36:25 +0200

    Add error check for scores command

--- scores.py
+++ scores.py
@@ -43,26 +43,34 @@ def get_scorecards_date(path, alias, date, date_to = ''):
 
 class Scores(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot    
+        self.bot = bot
+
+    # Checks
+    def has_scorecards():
+        def predicate(ctx):    
+            if utilities.is_path_empty(f'{ctx.guild.name}\{ctx.channel}'):
+                return False
+            else:            
+                return True
+        return commands.check(predicate)    
 
     @commands.group(pass_context=True, brief='Print stored scores', description='Prints all stored scorecards for this channel, including total')
-    async def scores(self, ctx):
-        # Check current scores stored in this channel
-        path = str(f'{ctx.guild.name}\{ctx.channel}')  
-        if utilities.is_path_empty(path):
-            await ctx.send('No scores stored for this channel')
-            return
-        
-        alias = Alias(ctx.guild.name)
-        alias.parse()
-
+    @has_scorecards()
+    async def scores(self, ctx):    
         if ctx.invoked_subcommand is None:
-            scorecards = get_scorecards(path, alias)
+            alias = Alias(ctx.guild.name)
+            alias.parse()
 
+            scorecards = get_scorecards(f'{ctx.guild.name}\{ctx.channel}', alias)
             if (scorecards.scorecards):               
                 await ctx.send(embed=scorecards.get_embed(ctx.author.avatar_url))  
             else:
                 await ctx.send("No courses found")
+
+    @scores.error
+    async def scores_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.send('No scores stored for this channel')
 
     @scores.group(pass_context=True, brief='scores related to a course')
     async def course(self, ctx):
@@ -71,11 +79,7 @@ class Scores(commands.Cog):
             return  
 
     @course.command(pass_context=True, name='list', brief='List courses', description='Lists courses stored in this channel')
-    async def course_list(self, ctx):
-        if utilities.is_path_empty(f'{ctx.guild.name}\{ctx.channel}'):
-            await ctx.send("No scores stored for this channel")
-            return
-    
+    async def course_list(self, ctx):    
         course_list = []
         for file in os.listdir(f'{ctx.guild.name}\{ctx.channel}'):
             if file.endswith(".csv"):
@@ -83,7 +87,6 @@ class Scores(commands.Cog):
                 scorecard = scorecard_reader.parse()
 
                 course_name = scorecard.coursename
-
                 if course_name not in course_list:
                     course_list.append(course_name)
     
@@ -103,8 +106,7 @@ class Scores(commands.Cog):
         alias = Alias(ctx.guild.name)
         alias.parse()
     
-        scorecards = get_scorecards_course(str(f'{ctx.guild.name}\{ctx.channel}'), alias, arg)           
-
+        scorecards = get_scorecards_course(str(f'{ctx.guild.name}\{ctx.channel}'), alias, arg)
         if (scorecards.scorecards):               
             await ctx.send(embed=scorecards.get_embed(ctx.author.avatar_url)) 
         else:
@@ -116,26 +118,20 @@ class Scores(commands.Cog):
             await ctx.send('Missing subcommands, see %help scores dates')
             return 
 
-
     @dates.command(pass_context=True, name='list', brief='List dates', description='Lists all dates for scorecards stored in this channel')
-    async def date_list(self, ctx):
-        if utilities.is_path_empty(f'{ctx.guild.name}\{ctx.channel}'):
-            await ctx.send("No scores stored for this channel")
-            return
-    
-        datelist = []
+    async def date_list(self, ctx):    
+        date_list = []
         for file in os.listdir(f'{ctx.guild.name}\{ctx.channel}'):
             if file.endswith(".csv"):
                 scorecard_reader = ScorecardReader(f'{ctx.guild.name}\{ctx.channel}', file)
                 scorecard = scorecard_reader.parse()
 
                 date = scorecard.date_time.date()
-
-                if date not in datelist:
-                    datelist.append(date)
+                if date not in date_list:
+                    date_list.append(date)
     
         msg = ''
-        for date in datelist:
+        for date in date_list:
             msg += f'\n{date}'
 
         await ctx.send(msg)
@@ -143,10 +139,6 @@ class Scores(commands.Cog):
     
     @dates.command(pass_context=True, name='search', brief='Search for scorecards', description='Search and print scorecards for date(s) given\nSearch one date: 1.1.1990\nSearch between two dates: 1.1.1990 1.12.1990')
     async def dates_search(self, ctx, *args):
-        if utilities.is_path_empty(f'{ctx.guild.name}\{ctx.channel}'):
-            await ctx.send("No scores stored for this channel")
-            return
-
         if len(args) == 0:
             await ctx.send("Missing date(s)")
             return
