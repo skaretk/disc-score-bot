e52fcca80bd53e6af09953428d00e0a6274dd362
Kjetil Skaret <kskaret@gmail.com>
2021-08-31 12:42:22 +0200

    Move output of the scrapers into own function to remove duplication

--- discs.py
+++ discs.py
@@ -20,24 +20,7 @@ class Discs(commands.Cog):
         for disc_scraper in scraper_list:
             self.discs.extend(disc_scraper.discs)
 
-
-    @commands.command(aliases=['d'], brief='Search for disc (Norway & VOEC)', description='Search for disc in norwegian and VOEC approved sites')
-    async def disc(self, ctx, *args, sep=" "):
-        self.discs = []
-        if len(args) == 0:
-            await ctx.send('No disc specified, see %help disc search')
-            return
-
-        search = sep.join(args)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for discs online"))        
-        
-        scrapers = scraper.Scrapers(search)
-        scraper_list = []
-        scraper_list.extend(scrapers.norwegian)
-        scraper_list.extend(scrapers.voec)
-
-        await self.scrape(scraper_list)
-
+    async def print_discs(self, ctx):
         if(len(self.discs) == 0):
             await ctx.send(f'Found no discs in stock {ctx.author.mention}')
             return        
@@ -58,7 +41,25 @@ class Discs(commands.Cog):
             await ctx.send('https://giphy.com/embed/32mC2kXYWCsg0')
             await ctx.send(f'WOW {ctx.author.mention}, thats a lot of {search} discs! ({len(self.discs)}!) ')
 
-        await self.bot.change_presence(activity=discord.Game(name="Disc golf"))       
+        await self.bot.change_presence(activity=discord.Game(name="Disc golf")) 
+
+    @commands.command(aliases=['d'], brief='Search for disc (Norway & VOEC)', description='Search for disc in norwegian and VOEC approved sites')
+    async def disc(self, ctx, *args, sep=" "):
+        self.discs = []
+        if len(args) == 0:
+            await ctx.send('No disc specified, see %help disc search')
+            return
+
+        search = sep.join(args)
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for discs online"))        
+        
+        scrapers = scraper.Scrapers(search)
+        scraper_list = []
+        scraper_list.extend(scrapers.norwegian)
+        scraper_list.extend(scrapers.voec)
+
+        await self.scrape(scraper_list)
+        await self.print_discs(ctx)
 
     @commands.command(name='disc_all', aliases=['d_a'], brief='List discs from all scrapers', description='Lists all discs in store for all sites added')
     async def disc_all(self, ctx, *args, sep=" "):
@@ -77,28 +78,7 @@ class Discs(commands.Cog):
         scraper_list.extend(scrapers.international)
 
         await self.scrape(scraper_list)
-
-        if(len(self.discs) == 0):
-            await ctx.send(f'Found no discs in stock {ctx.author.mention}')
-            return        
-        elif(len(self.discs)) == 1:
-            embed_title = f'Found {len(self.discs)} Disc!'            
-        else:
-            embed_title = f'Found {len(self.discs)} Discs!'
-        embed = discord.Embed(title=embed_title, color=0xFF5733)
-
-        for disc in self.discs:            
-            embed.add_field(name=disc.name, value=f'{disc.manufacturer}\nPrice: {disc.price}\n[{disc.store}]({disc.url})')
-        embed.set_thumbnail(url=(ctx.author.avatar_url))
-
-        if (len(embed) < 6000): # Size limit for embeds
-            await ctx.send(embed=embed)
-        else:
-            print(len(embed))
-            await ctx.send('https://giphy.com/embed/32mC2kXYWCsg0')
-            await ctx.send(f'WOW {ctx.author.mention}, thats a lot of {search} discs! ({len(self.discs)}!) ')
-
-        await self.bot.change_presence(activity=discord.Game(name="Disc golf"))
+        await self.print_discs(ctx)
 
     @commands.command(name='disc_flight', aliases=['d_f'], brief='Disc flightpath', description='Gets the flightpath of the specified disc')
     async def disc_flight(self, ctx, *args, sep=" "):
@@ -121,4 +101,4 @@ class Discs(commands.Cog):
             embed.set_thumbnail(url=(ctx.author.avatar_url))
             await ctx.send(embed=embed)            
         else:
-            await ctx.send(f'Could not find flight path for {search} {ctx.author.mention}')  
+            await ctx.send(f'Could not find flight path for {search} {ctx.author.mention}')
