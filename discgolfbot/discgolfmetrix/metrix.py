8c32489de887e3c52d30feaea4b182cb05b23dc8
Kjetil Skaret <kskaret@gmail.com>
2022-06-30 15:44:09 +0200

    Add slash command for metrix competitions #67

--- discgolfbot/discgolfmetrix/metrix.py
+++ discgolfbot/discgolfmetrix/metrix.py
@@ -1,9 +1,10 @@
-from datetime import datetime
-import time
-from nextcord import Interaction, SlashOption
 import nextcord
-from nextcord.ext import commands
+import time
 from apis.discgolfmetrixapi import DiscgolfMetrixApi
+from datetime import datetime
+from nextcord import Interaction, SlashOption
+from nextcord.ext import commands
+from typing import Optional
 from .metrixplayer import MetrixPlayer
 from .metrixcompetition import MetrixCompetition
 from .metrixcompetitions import MetrixCompetitions
@@ -40,7 +41,7 @@ class Metrix(commands.Cog):
         else:
             await ctx.send(f'Could not find the course')
 
-    @metrix.command(brief='List my competitions, or a players competitions', description='Lists out competitions for you, or a given player')
+    @metrix.command(brief='List competitions', description='Lists your competitions, or for a given player')
     async def competitions(self, ctx, user: nextcord.Member=None ):
         start_time = time.time()
         if (user == None):
@@ -87,3 +88,38 @@ class Metrix(commands.Cog):
             await interaction.response.send_message(f'Modified your metrix player code {user.mention}')
         else:
             await interaction.response.send_message(f'Added your metrix player code {user.mention}')
+
+    @metrix_slash_command.subcommand(name="competitions", description="Lists your competitions, or for a given player")
+    async def competitions_slash_command(
+        self,
+        interaction: Interaction,
+        user: Optional[nextcord.Member] = SlashOption(name="player", description="Player name", required=False)
+    ):
+        await interaction.response.defer()
+
+        start_time = time.time()
+        if (user == None):
+            user = interaction.user
+        player = MetrixPlayer(interaction.guild, user.display_name)
+        code = player.get_player_code()
+
+        if (code is not None):
+            api = DiscgolfMetrixApi()
+            json = api.my_competitions(code)
+            competitions = MetrixCompetitions()
+            for competition in json.get("my_competitions"):
+                comptetition_json = api.get_results(competition)
+                if comptetition_json is not None:
+                    metrix_competition = MetrixCompetition(comptetition_json)
+                    if metrix_competition.is_valid():
+                        if datetime.today().date() <= metrix_competition.datetime.date():
+                            competitions.add_competition(metrix_competition)
+
+            embed = competitions.get_embed()
+
+            await interaction.followup.send(embed=embed)
+        else:
+            await interaction.followup.send(f'Could not find your player code {user.display_name}, please add it by using %metrix add_player code')
+
+        cmd_time = time.time() - start_time
+        print(f'competitions: {cmd_time}')
