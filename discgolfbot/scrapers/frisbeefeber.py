684119c26224d092ac4bb8d1cb973d3bc7d485f0
Kjetil Skaret <kskaret@gmail.com>
2022-07-06 09:37:16 +0200

    fix frisbeefeber img scraping

--- discgolfbot/scrapers/frisbeefeber.py
+++ discgolfbot/scrapers/frisbeefeber.py
@@ -22,23 +22,29 @@ class DiscScraper(FrisbeeFeber):
         soup = self.urllib_get_beatifulsoup()
 
         for product in soup.select('li[class*="product-box-id-"]'):
-            # In stock ?
+            # In stock?
             not_in_stock = product.find("div", class_="product not-in-stock-product")
             if (not_in_stock is not None):
                 continue
-            disc = Disc()
-            disc.name = product.find("a", class_="title col-md-12").getText()
+
+            product_name = product.find("a", class_="title col-md-12").getText()
             # Search engine gives false results, check if the disc name is correct
-            if re.search(self.search, disc.name, re.IGNORECASE) is None:
+            if re.search(self.search, product_name, re.IGNORECASE) is None:
                 continue
+
             div_manufacturer = product.find("div", class_="manufacturer-box")
             alt_manufacturer = div_manufacturer.find("img", alt=True)
+            span_image = product.find("span", class_="image-mainimage")
+            img = span_image.find("img", src=True)
+            a_url = product.find('a', href=True)
+
+            disc = Disc()
+            disc.name = product_name
             disc.manufacturer = alt_manufacturer['alt']
             disc.price = product.find("div", class_="price col-md-12").getText().replace('\n','').replace('\t', '').strip()
+            disc.img = img["src"]
             disc.store = self.name
-            url = product.find('a', href=True)
-            disc.url = url['href']
-
+            disc.url = a_url['href']
             self.discs.append(disc)
         self.scraper_time = time.time() - start_time
         print(f'FrisbeeFeber scraper: {self.scraper_time}')
