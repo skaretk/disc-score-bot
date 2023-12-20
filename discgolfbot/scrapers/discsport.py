fb17cadc5d2a4b3ca1819f4e5028b532b74a82c4
Kjetil Skaret <kskaret@gmail.com>
2023-12-20 23:59:51 +0100

    fix discsport scraper

--- discgolfbot/scrapers/discsport.py
+++ discgolfbot/scrapers/discsport.py
@@ -19,21 +19,19 @@ class DiscScraper(Discsport):
         start_time = time.time()
         soup = self.urllib_get_beatifulsoup()
 
-        products = soup.find('div', class_="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 justify-content-center g-2 g-md-3 my-4")
-        if (products is not None):
-            for product in products.findAll("div", class_="position-relative mx-auto text-center p-0 m-0"):
-                disc = Disc()
-                a = product.find("h2", class_="h5 mt-2 mb-0").find('a', href=True)
-                disc.name = a.getText().replace("\n", " ").replace("\t", " ").strip()
-                disc.url = a["href"]
-                #manufacturer = re.search(r"]<br/>(.*?)\|", a["title"]).group(1).replace("\xa0", "")
-                #if (manufacturer is not None):
-                #    disc.manufacturer = manufacturer
-                img = product.find("img", class_="lozad")
-                if (img is not None):
-                    disc.img = img["data-src"]
-                disc.price = product.find("p", class_="h5 mt-1").getText().replace(':', ',')
-                disc.store = self.name
-                self.discs.append(disc)
+        for product in soup.findAll("div", class_="position-relative mb-1 mx-auto text-center"):
+            disc = Disc()
+            a = product.find("h2", class_="h5 fw-bold mb-1").find('a', href=True)
+            disc.name = a.getText().replace("\n", " ").replace("\t", " ").strip()
+            disc.url = a["href"]
+            #manufacturer = re.search(r"]<br/>(.*?)\|", a["title"]).group(1).replace("\xa0", "")
+            #if (manufacturer is not None):
+            #    disc.manufacturer = manufacturer
+            img = product.find("img", class_="lozad")
+            if img is not None:
+                disc.img = img["data-src"]
+            disc.price = product.find("p", class_="h5 mt-1").getText().replace(':', ',')
+            disc.store = self.name
+            self.discs.append(disc)
         self.scraper_time = time.time() - start_time
         print(f'Discsport scraper: {self.scraper_time}')
